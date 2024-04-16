7c1a7e2175cfbf003411c177f433e9346aa16bbb
Yasin Tanış <164824707+ysntns@users.noreply.github.com>
2024-04-17 02:00:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # Logo ve başlık
-st.sidebar.image(r"C:\Users\alcin\OneDrive\Masaüstü\projeMindMingle\logo.jpg", use_column_width=True)
+st.sidebar.image("logo.jpg", use_column_width=True)
 st.sidebar.title("EUREKA")
 
 # Ana başlık
